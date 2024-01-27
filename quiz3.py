d99ac1aa3cad6b43e8abaec6dc451cee484ee9e4
Jayme Greer <jaymgre@siue.edu>
2024-01-26 20:50:07 -0600

    first commit

---
+++ quiz3.py
@@ -0,0 +1,9 @@
+x = float(input("What is your first number? "))
+y = float(input("What is your second number? "))
+
+if x>y:
+    max = x
+else:
+    max = y
+
+print("The bigger number is " + str(max))
